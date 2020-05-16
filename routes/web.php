37a911a356282722ff5362f9fb47e70fd08a7989
Andrew Stanton <up683693@myport.ac.uk>
2020-05-16 10:37:14 -0400

    Adding authentication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/quizzes', 'QuizController@show');
 Route::post('/quizzes', 'QuizController@create');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
